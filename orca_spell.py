61a601f9be0c1278eea1d9b9abf22dfe8e0ed359
Cris <crisbento@alu.ufc.br>
2026-05-18 07:16:12 -0300

    add novas funções

--- orca_spell.py
+++ orca_spell.py
@@ -16,7 +16,7 @@ class OrcaSpellPlugin(GObject.Object, Gedit.WindowActivatable):
 
     #primeiro método rodará quando o professor ativa o plugin nas configurações do pluma
     def do_activate(self):
-        pass  
+        self.adicionar_atalhos() 
     #segundo método rodará quando o professor desativar o plugin
     def do_deactivate(self):
         pass
@@ -30,10 +30,10 @@ class OrcaSpellPlugin(GObject.Object, Gedit.WindowActivatable):
         #função add actions da biblioteca Gtk. 
         action_group.add_actions([
             #atalhos do grupo
-            ("IniciarRevisao", None, "Iniciar Revisão" "<Alt>e", None, self.iniciar_revisao),
+            ("IniciarRevisao", None, "Iniciar Revisão", "<Alt>e", None, self.iniciar_revisao),
             ("ProximoErro", None, "Próximo Erro", "<Alt>n", None, self.proximo_erro),
             ("ErroAnterior", None, "Erro Anterior", "<Alt>p", None, self.erro_anterior),
-            ("AceitarSugestao", None, "Aceitar Sugestão" "<Alt>s", None, self.aceitar_sugestao),
+            ("AceitarSugestao", None, "Aceitar Sugestão", "<Alt>s", None, self.aceitar_sugestao),
             ("IgnorarErro", None, "Ignorar Erro", "<Alt>i", None, self.ignorar_erro),
         ])
 
@@ -81,4 +81,63 @@ class OrcaSpellPlugin(GObject.Object, Gedit.WindowActivatable):
         self.erros.sort(key=lambda x: x["posicao"])
         self.indice = 0
         self.orca_fala(f"{len(self.erros)} erros encontrados. Pressione Alt N para o primeiro")
+    
+
+    def anunciar_erro_atual(self):
+        if not self.erros:
+            self.falar("Nenhum erro para navegar.")
+            return
+        erro = self.erros[self.indice]
+        palavra = erro ["palavra"]
+
+        #sugestão de correção
+        resultado = subprocess.run(
+            ["hunspell", "-d", "pt_BR"],
+            input = palavra, capture_output=True, text=True
+        )
+        sugestao = "sem sugestão"
+        for linha in resultado.stdout.split("\n"):
+            if linha.startswith("&"):
+                partes = linha.split(":")
+                if len(partes) > 1:
+                    sugestao = partes[1].strip().split(",")[0].strip()
+                    break
+        self.falar(
+            f"Erro {self.indice + 1} de {len(self.erros)}."
+            f"Palavra: {palavra}. Sugestão: {sugestao}."
+        )
+
+
+    def proximo_erro(self, action):
+        if not self.erros:
+            self.falar("Inicie a revisão primeiro com Alt E.")
+            return
+        if self.indice < len(self.erros) - 1:
+            self.indice += 1 
+        else:
+            self.falar("Você chegou ao último erro.")
+            return
+        self.anunciar_erro_atual()
+
+    
+    def erro_anterior(self, action):
+        if not self.erros:
+            self.falar("Inicie a revisão primeiro com Alt E.")
+            return
+        if self.indice > 0:
+            self.indice -= 1
+        else:
+            self.falar("Você está no primeiro erro.")
+            return
+        self.anunciar_erro_atual()
+    
+    def aceitar_sugestao(self, action):
+        self.falar("Função aceitar sugestão em desenvolvimento.")
+
+    
+    def ignorar_erro(self, action):
+        if not self.erros:
+            return
+        self.falar("Erro ignorado")
+        self.proximo_erro(action)
 
